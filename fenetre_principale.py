936cc033f812cc3307b186e7b63d89124264ce65
2440391 <2440391@cegeplimoilou.ca>
2025-10-09 13:21:53 -0400

    (avec loic) base de fenetre_principale.py

---
+++ fenetre_principale.py
@@ -0,0 +1,43 @@
+from PyQt6.QtWidgets import QMainWindow, QLineEdit, QSlider, QVBoxLayout, QRadioButton, QPushButton, QMessageBox
+from fonction_integre import FonctionModel
+from PyQt6.uic import loadUi
+
+from mpl_canvas import MPLCanvas
+
+
+class FonctionView(QMainWindow):
+    fonctionLineEdit : QLineEdit
+    borneInfLineEdit : QLineEdit
+    borneSupLineEdit : QLineEdit
+    canvas_layout : QVBoxLayout
+    nombreSlider : QSlider
+    gaucheRadioButton : QRadioButton
+    droiteRadioButton : QRadioButton
+    calculerPushButton : QPushButton
+    exporterPushButton : QPushButton
+    sommeLineEdit : QLineEdit
+    integraleLineEdit : QLineEdit
+
+    __model : FonctionModel
+
+    def __init__(self):
+        super().__init__()
+        loadUi("ui/function_view.ui", self)
+
+        self.model = FonctionModel()
+        canvas = MPLCanvas(self.model)
+        self.canvas_layout.addWidget(canvas)
+
+        self.fonctionLineEdit.editingFinished.connect(self.fonction_edit, canvas.dessiner)
+
+
+    def fonction_edit(self):
+        fonct_str = self.fonctionLineEdit.text()
+        if self.model.validate_fonction(fonct_str) :
+            self.model.fonction = fonct_str
+
+        else :
+            QMessageBox.critical(self, "Erreur", "La fonction est invalide")
+            self.fonctionLineEdit.clear()
+
+
