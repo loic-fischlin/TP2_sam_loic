717ca9557fdcf90dbe31d8d963630a1fe2ff581e
2446444 <2446444@cegeplimoilou.ca>
2025-10-20 13:53:57 -0400

    fin cours lundi 20 octobre

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import sys
+import traceback
 
 from PyQt6.QtWidgets import QApplication
 from fenetre_principale import FonctionView
 
 if __name__ == "__main__":
+    def qt_exception_hook(exctype, value, tb):
+        traceback.print_exception(exctype, value, tb)
+
+    sys.excepthook = qt_exception_hook
     app = QApplication(sys.argv)
     fenetre = FonctionView()
     fenetre.show()
