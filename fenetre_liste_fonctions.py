c8af5b333b3b1f223254389e583b1de617e7f424
loicf <2446444@cegeplimoilou.ca>
2025-10-26 14:47:00 -0400

    finitions pour rendre le projet fonctionnel (enregistrer, supprimer, ajouter, json)

--- fenetre_liste_fonctions.py
+++ fenetre_liste_fonctions.py
@@ -1,6 +1,5 @@
-import json
 import os.path
-
+import subprocess
 from PyQt6.QtCore import pyqtSignal, Qt
 from PyQt6.QtWidgets import QDockWidget, QPushButton, QLineEdit, QMessageBox, QListView
 from PyQt6.uic import loadUi
@@ -8,16 +7,14 @@ from fonction_integre import FonctionModel
 from liste_fonctions import ListeFonctionsModel
 
 
-
 class ListeFonctionView(QDockWidget):
-    ajouterPushButton : QPushButton
-    supprimerPushButton : QPushButton
-    enregistrerPushButton : QPushButton
-    fonctionLineEditDock : QLineEdit
-    listViewFonctions : QListView
+    ajouterPushButton: QPushButton
+    supprimerPushButton: QPushButton
+    enregistrerPushButton: QPushButton
+    fonctionLineEditDock: QLineEdit
+    listViewFonctions: QListView
 
-
-    __listeModele : ListeFonctionsModel
+    __listeModele: ListeFonctionsModel
     liste_modifiee = pyqtSignal()
 
     def __init__(self, modele_partage):
@@ -25,7 +22,6 @@ class ListeFonctionView(QDockWidget):
         loadUi("ui/dockwidget.ui", self)
         self.setWindowTitle("Liste des fonctions")
         self.setAllowedAreas(Qt.DockWidgetArea.RightDockWidgetArea | Qt.DockWidgetArea.LeftDockWidgetArea)
-
 
         self.__listeModele = modele_partage
         self.listViewFonctions.setModel(self.__listeModele)
@@ -39,17 +35,6 @@ class ListeFonctionView(QDockWidget):
         self.supprimerPushButton.clicked.connect(self.supprimer_fonction)
         self.enregistrerPushButton.clicked.connect(self.enregistrer_donnees)
 
-
-    def activer_bouton_ajouter(self):
-        if self.fonctionLineEditDock.text():
-            self.ajouterPushButton.setEnabled(True)
-
-    def activer_bouton_supprimer(self):
-        if self.listViewFonctions.currentIndex().isValid():
-            self.supprimerPushButton.setEnabled(True)
-        else:
-            self.supprimerPushButton.setEnabled(False)
-
     def enregistrer_donnees(self):
         try:
             chemin_fichier = "json/fonctions.json"
@@ -60,12 +45,15 @@ class ListeFonctionView(QDockWidget):
         except Exception as e:
             QMessageBox.critical(self, "Erreur", f"Impossible d’enregistrer le fichier : {str(e)}")
 
+    def activer_bouton_ajouter(self):
+        if self.fonctionLineEditDock.text():
+            self.ajouterPushButton.setEnabled(True)
 
-
-
-    def closeEvent(self, event):
-        event.ignore()
-        self.hide()
+    def activer_bouton_supprimer(self):
+        if self.listViewFonctions.currentIndex().isValid():
+            self.supprimerPushButton.setEnabled(True)
+        else:
+            self.supprimerPushButton.setEnabled(False)
 
     def ajouter_fonction(self):
         texte = self.fonctionLineEditDock.text()
@@ -83,7 +71,6 @@ class ListeFonctionView(QDockWidget):
         self.supprimerPushButton.setEnabled(False)
 
 
-
-
-
-
+    def closeEvent(self, event):
+        event.ignore()
+        self.hide()
